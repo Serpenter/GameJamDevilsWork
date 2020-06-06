7c80b764edc8869775cf3141bf0d55a393c124eb
MykolaHoretskyi <kolan-777@ukr.net>
2020-06-07 01:44:33 +0300

    tabs to spaces nun and whip

--- GameJamDevilsWork/prefab_scenes/Nun/Nun.gd
+++ GameJamDevilsWork/prefab_scenes/Nun/Nun.gd
@@ -34,26 +34,26 @@ var pray_point_time = 3.0
 var states = ["idle", "go_to_exit", "pray", "ascension"]
 
 var state_upgrades = {
-	"go_to_exit":"idle",
-	"idle":"pray"}
-	
+    "go_to_exit":"idle",
+    "idle":"pray"}
+    
 var state_downgrades = {
-	"idle":"go_to_exit",
-	"pray":"idle"}
+    "idle":"go_to_exit",
+    "pray":"idle"}
 
 var current_state
 
 
 var state_min_time = {
-	"idle":5,
-	"go_to_exit":5,
-	"pray": 1 # not_used since not upgradable
+    "idle":5,
+    "go_to_exit":5,
+    "pray": 1 # not_used since not upgradable
 }
 
 var state_range_time = {
-	"idle":5,
-	"go_to_exit":10,
-	"pray": 1 # not_used since not upgradable
+    "idle":5,
+    "go_to_exit":10,
+    "pray": 1 # not_used since not upgradable
 }
 
 var idle_time_max = 15.0
@@ -75,26 +75,26 @@ func _ready():
     
 
 func get_in_pot():
-	if current_state == "go_to_pot":
-		print("SINNER ENTERS THE POT")
-		queue_free()
-		return true
-	else:
-		print("SINNER WON'T GO IN THE POT")
-		return false
-		
+    if current_state == "go_to_pot":
+        print("SINNER ENTERS THE POT")
+        queue_free()
+        return true
+    else:
+        print("SINNER WON'T GO IN THE POT")
+        return false
+        
 func exit_hell():
-	if current_state == "go_to_exit":
-		print("SINNER EXITED HELL")
-		current_state = "ascension"
-		$CollisionShape2D.disabled = true
-		$AnimationPlayer.play("Ascension")
-		return true
-	else:
-		print("SINNER WON'T EXIT HELL")
-		return false
-	
-	
+    if current_state == "go_to_exit":
+        print("SINNER EXITED HELL")
+        current_state = "ascension"
+        $CollisionShape2D.disabled = true
+        $AnimationPlayer.play("Ascension")
+        return true
+    else:
+        print("SINNER WON'T EXIT HELL")
+        return false
+    
+    
 func push_sinner(direction):
 #    move_vector = Vector2(1,0).rotated(direction)
 #    move_vector = move_vector.normalized()
@@ -112,57 +112,57 @@ func push_sinner(direction):
         move_speed = max_move_speed
     
 func go_to_node_in_group(group_name):
-	var group_objects = get_tree().get_nodes_in_group(group_name)
-	var chosen_object = group_objects[randi() % group_objects.size()]
-	var end_position = chosen_object.get_global_position()
-	var start_position = self.get_global_position()
-	path = nav_2d.get_simple_path(start_position, end_position)
-	
-	if DEBUG:
-		debug_line.points = path   
-		print("start ", start_position)
-		print("end ", end_position)
-		print(debug_line.points)
-	
-	
+    var group_objects = get_tree().get_nodes_in_group(group_name)
+    var chosen_object = group_objects[randi() % group_objects.size()]
+    var end_position = chosen_object.get_global_position()
+    var start_position = self.get_global_position()
+    path = nav_2d.get_simple_path(start_position, end_position)
+    
+    if DEBUG:
+        debug_line.points = path   
+        print("start ", start_position)
+        print("end ", end_position)
+        print(debug_line.points)
+    
+    
 func update_state(delta):
-	if not current_state in state_upgrades.keys():
-		return
-		
-	state_change_timeout -= delta
-	
-	if state_change_timeout <= 0:
-		upgrade_state()
+    if not current_state in state_upgrades.keys():
+        return
+        
+    state_change_timeout -= delta
+    
+    if state_change_timeout <= 0:
+        upgrade_state()
    
-	 
+     
 func upgrade_state():
-	state_change_timeout = state_change_time
-	
-	if not current_state in state_upgrades.keys():
-		return
+    state_change_timeout = state_change_time
+    
+    if not current_state in state_upgrades.keys():
+        return
 
-	set_state(state_upgrades[current_state])
+    set_state(state_upgrades[current_state])
  
    
 func downgrade_state():   
-	state_change_timeout = state_change_time
-	
-	if not current_state in state_downgrades.keys():
-		return
+    state_change_timeout = state_change_time
+    
+    if not current_state in state_downgrades.keys():
+        return
 
-	set_state(state_downgrades[current_state])
-		
+    set_state(state_downgrades[current_state])
+        
 
 func set_state(new_state):
 
-	if not new_state in states:
-		print("ERROR, state not found")
-		print(new_state)
-		return
+    if not new_state in states:
+        print("ERROR, state not found")
+        print(new_state)
+        return
 
-	current_state = new_state
-	
-	path = []
+    current_state = new_state
+    
+    path = []
 
     if current_state == "go_to_exit":
         go_to_node_in_group("exits")
@@ -184,48 +184,48 @@ func set_state(new_state):
 
 
 func is_angel_present():
-	return len(get_tree().get_nodes_in_group("angels")) > 0
+    return len(get_tree().get_nodes_in_group("angels")) > 0
 
 func resolve_path_completion():
-	if current_state == "idle":
-		print("ERROR idle path completion")
-		return
-		
-		
+    if current_state == "idle":
+        print("ERROR idle path completion")
+        return
+        
+        
 func update_move_speed(delta):
-	if move_speed > min_move_speed:
-		move_speed = max(move_speed - delta * speed_deacceleration, min_move_speed)
-		
-		
+    if move_speed > min_move_speed:
+        move_speed = max(move_speed - delta * speed_deacceleration, min_move_speed)
+        
+        
 func stun(duration):
-	if current_state == "ascension":
-		return false
+    if current_state == "ascension":
+        return false
 
-	if stun_time < duration:
-		stun_time = duration
-		
-	enable_stun()
-	
-	return true
-		
-		
+    if stun_time < duration:
+        stun_time = duration
+        
+    enable_stun()
+    
+    return true
+        
+        
 func enable_stun():
-	animation_player.play("stun")
-	
+    animation_player.play("stun")
+    
     if holy_area:
         holy_area.disable()
     
 func disable_stun():
-	animation_player.play("idle")
-	
+    animation_player.play("idle")
+    
     if current_state == "pray" and holy_area:
         holy_area.enable()
     
 
 func update_idle():
-	if animation_player.current_animation != "idle":
-			animation_player.play("idle")
-	
+    if animation_player.current_animation != "idle":
+            animation_player.play("idle")
+    
 func update_prayer(delta):
 
     if animation_player.current_animation != "pray":
@@ -290,5 +290,5 @@ func _process(delta):
 
 
 func _on_AnimationPlayer_animation_finished(anim_name):
-	if anim_name == "Ascension":
-		queue_free()
+    if anim_name == "Ascension":
+        queue_free()
