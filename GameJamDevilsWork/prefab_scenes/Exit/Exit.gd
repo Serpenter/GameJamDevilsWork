dbcb052cf8862a4c7a83814048d13d7e30aedc0e
Kostiantyn <knifantiev@gmail.com>
2020-06-07 02:07:15 +0300

    Fixed first real level

--- GameJamDevilsWork/prefab_scenes/Exit/Exit.gd
+++ GameJamDevilsWork/prefab_scenes/Exit/Exit.gd
@@ -16,29 +16,29 @@ signal imp_in_holy_place(is_entering)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    connect("sinner_escaped", get_tree().get_root().get_node("MainGame/CanvasLayer/HUD"), "_on_sinner_escaped")
-    connect("sinner_escaped", get_tree().get_root().get_node("MainGame"), "_on_sinner_escaped")
-    connect("imp_in_holy_place", get_tree().get_root().get_node("MainGame"), "_on_imp_in_holy_place")
+	connect("sinner_escaped", get_tree().get_root().get_node("MainGame/CanvasLayer/HUD"), "_on_sinner_escaped")
+	connect("sinner_escaped", get_tree().get_root().get_node("MainGame"), "_on_sinner_escaped")
+	connect("imp_in_holy_place", get_tree().get_root().get_node("MainGame"), "_on_imp_in_holy_place")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-    pass
+	pass
 
 
 func _on_ExitArea_body_entered(colBody):
-    if colBody.has_method("exit_hell"):
-        if colBody.exit_hell():
-            sinners_exited += 1
-            audio_stream_player.play()
-            emit_signal("sinner_escaped", 1)
+	if colBody.has_method("exit_hell"):
+		if colBody.exit_hell():
+			sinners_exited += 1
+			audio_stream_player.play()
+			emit_signal("sinner_escaped", 1)
 
 
 func _on_Area2D_body_entered(body):
-    if body.name == "Imp":
-        emit_signal("imp_in_holy_place", true)
+	if body.name == "Imp":
+		emit_signal("imp_in_holy_place", true)
 
 
 func _on_Area2D_body_exited(body):
-    if body.name == "Imp":
-        emit_signal("imp_in_holy_place", false)
+	if body.name == "Imp":
+		emit_signal("imp_in_holy_place", false)
