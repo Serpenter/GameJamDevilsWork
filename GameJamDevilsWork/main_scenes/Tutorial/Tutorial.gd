aba1c7b4b137d3c27f54646ffd42fcc854cb9dc8
Kostiantyn <knifantiev@gmail.com>
2020-06-06 20:28:40 +0300

    Mostly done

--- GameJamDevilsWork/main_scenes/Tutorial/Tutorial.gd
+++ GameJamDevilsWork/main_scenes/Tutorial/Tutorial.gd
@@ -1,6 +1,10 @@
 extends Node2D
 
-var victory_condition = 2
+var current_stage = 0
+var stage_one_condition = 5
+var stage_two_condition = 25
+var stage_two_fail_condition = 5
+
 var total_sinners_punished = 0
 var total_sinners_escaped = 0
 
@@ -12,7 +16,12 @@ var sinner_prefab = preload("res://prefab_scenes/Sinner/Sinner.tscn")
 onready var respawns = $world/sinner_spawns
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$CanvasLayer/HUD.max_sinners_escaped = "infinity"
+	$CanvasLayer/HUD.target_sinners_punished = 5
+	if current_stage == 0:
+		$CanvasLayer/OnStartNotice.visible = true
+		current_stage = 1
+	get_tree().paused = true
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -21,9 +30,36 @@ func _ready():
 func _on_sinner_punished(change):
 	total_sinners_punished += change
 
-	if total_sinners_punished == victory_condition:
-		$CanvasLayer/PopupDialog.show()
+	if current_stage == 1 and total_sinners_punished >= stage_one_condition:
+		$world/obstacles/first.on_destroy()
+		current_stage = 2
+		total_sinners_escaped = 0
+		$CanvasLayer/HUD.total_sinners_escaped = 0
+		$CanvasLayer/HUD.max_sinners_escaped = 5
+		$CanvasLayer/HUD._on_sinner_escaped(0)
+		
+		$CanvasLayer/HUD.target_sinners_punished = stage_two_condition
+		
+		$world/sinner_spawns/respawn2.activate_spawn()
+		$world/sinner_spawns/respawn3.activate_spawn()
+		
+		$world/pots/second.is_working = true
+		$world/exits/SecondExit.is_working = true
+		
+		$CanvasLayer/OnSecondStageNotice.visible = true
+		max_sinners_in_game = 6
+		get_tree().paused = true
 
+	if current_stage == 2:
+		
+		if total_sinners_escaped >= stage_two_fail_condition:
+			$CanvasLayer/OnFailNotice.visible = true
+			get_tree().paused = true
+			
+		if total_sinners_punished >= stage_two_condition:
+			$CanvasLayer/OnWinNotice.visible = true
+			get_tree().paused = true
+		
 
 func _on_ToMainMenu_pressed():
 	$"/root/GSceneManager".goto_scene_wloader("res://main_scenes/MainMenu/MainMenu.tscn")
